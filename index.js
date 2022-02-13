3ce546025f5cdd4e5185528c0e06357d5ea008ee
Supaasara Bowman <supaasarabowman@Supaasaras-MacBook-Pro.local>
2022-02-12 18:24:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+messages = ['Thank you, ${arrayNames[i]}, for the wonderful surprise gift!'];
+arrayNames = ["Guadalupe", "Ollie", "Aki"];
+writeCards(["Guadalupe", "Ollie", "Aki"],"messages")
+  
+function writeCards(arrayNames, messages) {
+    const arrayMessages = []
+    for (let i = 0 ; i < arrayNames.length ; i++) {
+        arrayMessages.push( `Thank you, ${arrayNames[i]}, for the wonderful ${messages} gift!`)
+    }
+
+    
+    return arrayMessages;   
+
+}
+
+function countDown(number){
+    while (number > 0){
+        console.log(number)
+        number -= 1
+    }
+    console.log(number)
+}
+
+
+
